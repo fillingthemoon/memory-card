3c7e846ab1df9ae50e3671fe93db6690ace1954d
philemonheng <phil.hsy@outlook.com>
2021-01-24 23:39:54 +0800

    add instruction

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ const  App = () => {
     <div id="app-div">
       <h1>Memory Game</h1>
       <h3>GitHub repo <a href="https://github.com/fillingthemoon/memory-card" target="_blank" rel="noreferrer">here</a></h3>
+      <h3 id="instr">Click on each drink only once!</h3>
       <Scoreboard score={score}/>
       <Cards score={score} setScore={setScore} />
     </div>
